0aec3e846eff546cbd56fc80a12abbdd5e77b9cf
mohammad <malhumaidaan@gmail.com>
2022-10-12 16:34:50 +0300

    Finished

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Pokemon
 
 # Register your models here.
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ("id","name", "hp", "active",)
+    list_display_links = ("id", "name",)
+    list_filter = ("active",)
+    readonly_fields = ("created_at", "modified_at",)
+
+    fieldsets = (
+        (
+            "general", {
+                "fields": ("name", "hp", "active", "type",)
+            }
+        ),
+        (
+            "Localization", {
+                "fields": ("name_ar", "name_fr", "name_jp",), "classes": ("collapse",)
+            }
+        ),
+        (
+            None, {
+                "fields": ("created_at", "modified_at",)
+            }
+        )
+    )
+
--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,29 @@
+from email.policy import default
+from wsgiref.validate import validator
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
+
+class Pokemon(models.Model):
+
+    class PokemonType(models.TextChoices):
+        WATER = 'WA'
+        GRASS = 'GR'
+        GHOST = 'GH'
+        STEEL = 'ST'
+        FAIRY = 'FA'
+
+
+    name = models.CharField(max_length=30)
+    type = models.CharField(max_length=100,choices=PokemonType.choices)
+    hp = models.PositiveIntegerField(validators = [MinValueValidator(50),MaxValueValidator(350)])
+    active = models.BooleanField(default=True)
+    name_fr = models.CharField(max_length=30, default="", blank = True)
+    name_ar = models.CharField(max_length=30, default="", blank = True)
+    name_jp = models.CharField(max_length=30, default="", blank = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
